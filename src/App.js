c71781aa101cb2625983922cdb47444efae09978
Bob Wise <bobwise@quickenloans.com>
2019-12-18 16:02:00 -0500

    without spark commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.scss';
 
 function App() {
   return (
-    <main  class="sprk-o-CenteredColumn sprk-u-ptl">
-      <h1 class="sprk-b-TypeDisplayTwo sprk-b-PageTitle">Hello, Spark! 👋</h1>
-      <h1 class="sprk-b-TypeDisplaySix sprk-u-mtl">Add components here</h1>
-    </main>
+    <p>This is a boilerplate React app.</p>
   );
 }
 
