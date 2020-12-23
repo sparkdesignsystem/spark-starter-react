39a19a0af3594be820a3e0fa86cfe21423dc05ca
Wise <bobwise@quickenloans.com>
2020-12-23 12:02:54 -0500

    rollback to test ie11

--- src/routes/SprkInputDocs/SprkRevealInputDocs/SprkRevealInputDocs.js
+++ src/routes/SprkInputDocs/SprkRevealInputDocs/SprkRevealInputDocs.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { SprkRevealInput, sprkIsValidSSN, sprkFormatSSN }
+import { SprkRevealInput }
   from '@sparkdesignsystem/spark-react';
+
+  import { 
+    isValidSSN, formatSSN
+  } from '@sparkdesignsystem/spark/es5/sparkExports';
+
 import ExampleContainer from '../../../containers/ExampleContainer/ExampleContainer';
 
 class SprkRevealInputDocs extends Component {
@@ -33,8 +38,8 @@ class SprkRevealInputDocs extends Component {
         <ExampleContainer heading="SSN">
           <SprkRevealInput
             errorMessage="There is an error in this field."
-            formatter={sprkFormatSSN}
-            valid={sprkIsValidSSN(ssn)}
+            formatter={formatSSN}
+            valid={isValidSSN(ssn)}
             value={ssn}
             onChange={this.handleChange}
             label="Social Security Number"
